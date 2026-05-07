b3ee4b5663f7ce4092b0d1d7e440882d632b2a60
trustcbde-creator <trustcbde@gmail.com>
2026-05-07 17:31:59 +0300

    Update app.py

--- app.py
+++ app.py
@@ -41,4 +41,4 @@ def handle_message(data):
 if __name__ == '__main__':
     # Порт для хостинга берется из переменной окружения PORT
     port = int(os.environ.get("PORT", 5000))
-    socketio.run(app, host='0.0.0.0', port=port)
+    socketio.run(app, host='0.0.0.0', port=port, allow_unsafe_werkzeug=True)
