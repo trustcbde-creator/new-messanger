5de51b804b61cc149f19d53fc0a1c7d6e215944a
trustcbde-creator <trustcbde@gmail.com>
2026-05-07 18:32:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from flask import Flask, render_template, request
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "secret_key_123"
+app.config["SECRET_KEY"] = "secret"
 
 socketio = SocketIO(
     app,
@@ -14,45 +14,82 @@ socketio = SocketIO(
     async_mode="eventlet"
 )
 
+# {sid: username}
 users = {}
+
+# {username: sid}
+user_sids = {}
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+
 @socketio.on("join")
 def handle_join(username):
+
     users[request.sid] = username
+    user_sids[username] = request.sid
 
     emit(
         "update_users",
-        list(users.values()),
+        list(user_sids.keys()),
         broadcast=True
     )
+
 
 @socketio.on("disconnect")
 def handle_disconnect():
+
     if request.sid in users:
+
+        username = users[request.sid]
+
         del users[request.sid]
+
+        if username in user_sids:
+            del user_sids[username]
 
         emit(
             "update_users",
-            list(users.values()),
+            list(user_sids.keys()),
             broadcast=True
         )
 
-@socketio.on("send_message")
-def handle_message(data):
-    emit(
-        "receive_message",
-        {
-            "user": data["user"],
-            "text": data["text"]
-        },
-        broadcast=True
-    )
+
+@socketio.on("private_message")
+def handle_private_message(data):
+
+    target = data["target"]
+    sender = data["user"]
+    text = data["text"]
+
+    if target in user_sids:
+
+        target_sid = user_sids[target]
+
+        emit(
+            "receive_private_message",
+            {
+                "user": sender,
+                "text": text
+            },
+            room=target_sid
+        )
+
+        # Отправляем и себе
+        emit(
+            "receive_private_message",
+            {
+                "user": sender,
+                "text": text
+            },
+            room=request.sid
+        )
+
 
 if __name__ == "__main__":
+
     port = int(os.environ.get("PORT", 10000))
 
     socketio.run(
