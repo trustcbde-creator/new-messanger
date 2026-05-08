ba872abed13992301436d5d81d29797bdb503c14
trustcbde-creator <trustcbde@gmail.com>
2026-05-08 18:05:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,99 +1,69 @@
 import eventlet
-eventlet.monkey_patch()
 
-import os
-from flask import Flask, render_template, request
-from flask_socketio import SocketIO, emit
+    if 'user' in session:
 
-app = Flask(__name__)
-app.config["SECRET_KEY"] = "secret"
+        online_users[session['user']] = request.sid
 
-socketio = SocketIO(
-    app,
-    cors_allowed_origins="*",
-    async_mode="eventlet"
-)
-
-# {sid: username}
-users = {}
-
-# {username: sid}
-user_sids = {}
-
-@app.route("/")
-def index():
-    return render_template("index.html")
+        emit('online_update', broadcast=True)
 
 
-@socketio.on("join")
-def handle_join(username):
+@socketio.on('disconnect')
+def disconnect():
 
-    users[request.sid] = username
-    user_sids[username] = request.sid
+    if 'user' in session:
+
+        if session['user'] in online_users:
+            del online_users[session['user']]
+
+        emit('online_update', broadcast=True)
+
+
+@socketio.on('send_message')
+def send_message(data):
+
+    sender = session['user']
+
+    receiver = data['receiver']
+
+    text = data['text']
+
+    msg = Message(
+        sender=sender,
+        receiver=receiver,
+        text=text
+    )
+
+    db.session.add(msg)
+
+    db.session.commit()
+
+    payload = {
+        'sender': sender,
+        'text': text,
+        'time': datetime.utcnow().strftime('%H:%M')
+    }
+
+    if receiver in online_users:
+
+        emit(
+            'receive_message',
+            payload,
+            room=online_users[receiver]
+        )
 
     emit(
-        "update_users",
-        list(user_sids.keys()),
-        broadcast=True
+        'receive_message',
+        payload,
+        room=request.sid
     )
 
 
-@socketio.on("disconnect")
-def handle_disconnect():
+if __name__ == '__main__':
 
-    if request.sid in users:
-
-        username = users[request.sid]
-
-        del users[request.sid]
-
-        if username in user_sids:
-            del user_sids[username]
-
-        emit(
-            "update_users",
-            list(user_sids.keys()),
-            broadcast=True
-        )
-
-
-@socketio.on("private_message")
-def handle_private_message(data):
-
-    target = data["target"]
-    sender = data["user"]
-    text = data["text"]
-
-    if target in user_sids:
-
-        target_sid = user_sids[target]
-
-        emit(
-            "receive_private_message",
-            {
-                "user": sender,
-                "text": text
-            },
-            room=target_sid
-        )
-
-        # Отправляем и себе
-        emit(
-            "receive_private_message",
-            {
-                "user": sender,
-                "text": text
-            },
-            room=request.sid
-        )
-
-
-if __name__ == "__main__":
-
-    port = int(os.environ.get("PORT", 10000))
+    port = int(os.environ.get('PORT', 10000))
 
     socketio.run(
         app,
-        host="0.0.0.0",
+        host='0.0.0.0',
         port=port
     )
